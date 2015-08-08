f1fc56a6830df69b9a4078a7a042b665d051e631
SuburbanFilth <suburb4nfilth@gmail.com>
2015-08-08 20:03:22 +0300

    s.3
    added :
    - show databases
    - show tables
    - view table
    - view table info (only the logic)

--- explorer.py
+++ explorer.py
@@ -1,6 +1,7 @@
 import MySQLdb
 import pexpect
 import excep as ex
+import time
 
 # Start the server from the terminal sudo /usr/local/mysql/support-files/mysql.server start
 # Stop the server from the terminal sudo /usr/local/mysql/support-files/mysql.server stop
@@ -15,8 +16,9 @@ class Explorer(object):
 		#get the password for the connection object and to start the server
 		self.root_pass = root_password
 		#start the mysql server
-		self.start_server()
+		#self.start_server()
 		#create the connection
+		#time.sleep(3)
 		self.connection = MySQLdb.connect(host='',
 											user = self.root_user,
 											passwd = self.root_pass,
@@ -40,20 +42,60 @@ class Explorer(object):
 		try:
 			statement = 'DESCRIBE %s' % table_name
 			self.cursor.execute(statement)
+			to_return = {'Field':[],'Type':[],'Null':[],'Key':[],'Default':[],'Extra':[]}
+			records = self.cursor.fetchall()
+			for tupl in records:
+				to_return['Order'] = ('Field','Type','Null','Key','Default','Extra')
+				to_return['Field'].append(tupl[0])
+				to_return['Type'].append(tupl[1])
+				to_return['Null'].append(tupl[2])
+				to_return['Key'].append(tupl[3])
+				to_return['Default'].append(tupl[4])
+				to_return['Extra'].append(tupl[5])
+			return to_return
 		except Exception:
 			raise ex.TableDoesntExist(table_name)
+
+	def show_tables(self):
+		statement = 'SHOW TABLES'
+		self.cursor.execute(statement)
+		return {'Name': [x[0] for x in self.cursor.fetchall()]}
+
+	def show_table(self,table_name):
+		statement = 'SELECT * FROM {}'.format(table_name)
+		to_return = {}
+		column_statement = 'DESCRIBE {}'.format(table_name)
+		self.cursor.execute(column_statement)
+		checker = []
+		for tupl in self.cursor.fetchall():
+			to_return[tupl[0]] = []
+			checker.append(tupl[0])
+		self.cursor.execute(statement)
+		for tupl in self.cursor.fetchall():
+			for index in xrange(len(checker)):
+				to_return[checker[index]].append(tupl[index])
+		return to_return
+
+	def show_dbs(self):
+		statement = 'SHOW DATABASES'
+		self.cursor.execute(statement)
+		return  {'Name' : [x[0] for x in self.cursor.fetchall()]}
 
 
 	def start_server(self):
 		''' VERY IMPORTANT NOTE : If the server is started manually it has to be closed manually'''
 		process = pexpect.spawn('sudo /usr/local/mysql/support-files/mysql.server start')
-		process.expect('Password:')
-		process.send(self.root_pass)
+		try:
+			process.expect('Password:')
+			process.send(self.root_pass)
+		except pexpect.EOF:
+			pass
+		
 
 if __name__=='__main__':
 	p = Explorer('root','0899504274')
-	table_info = {'col1':('varchar','255')}
-	table_name = 'ssa'
-	p.create_table('ssa',table_info)
-	p.cursor.execute('SHOW tables')
-	print p.cursor.fetchall()
+	#table_info = {'col1':('varchar','255')}
+	#table_name = 'ssa'
+	#p.create_table('ssa',table_info)
+	print p.show_table('ssa')
+	
--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import Tkinter as tk
 from collections import OrderedDict
+from explorer import *
 
 class Gui(tk.Frame):
 
@@ -14,8 +15,10 @@ class Gui(tk.Frame):
 		self.main_display = tk.Frame(self)
 		
 
-		self.db_display = tk.Frame(self)
 		self.table_display = tk.Frame(self)
+
+		self.db_display = tk.Frame(self)
+		
 
 		self.main_options = (
 							('DB',(self.packer,(self.main_display,self.db_view))),
@@ -25,15 +28,15 @@ class Gui(tk.Frame):
 							)
 
 		self.db_options = (
-							('Show DB',(0,)),
+							('Show DBs',(self.show_dbs,)),
 							('Change DB',(0,)),
 							('Delete DB',(0,)),
 							('Back',(self.packer,(self.db_display,self.main_view)))
 						)
 
 		self.table_options = (
-								('Info',(0,)),
-								('View',(0,)),
+								('Tables',(self.show_tables,)),
+								('View',(self.view_table,)),
 								('Create',(0,)),
 								('Alter',(0,)),
 								('Delete',(0,)),
@@ -46,6 +49,10 @@ class Gui(tk.Frame):
 									('Table -','structure inside the database that contains rows and columns. Example : Person'),
 									('Column -',' defines the different qualities of the table. Example : Name, Age etc.'),
 									('Row -',' contains the different records. Example : Ron, 34 , etc.'))
+
+		self.explorer = Explorer('root','0899504274')
+
+		self.table_var = tk.StringVar()
 
 		self.main_view()
 
@@ -60,12 +67,24 @@ class Gui(tk.Frame):
 				tk.Button(frame,text=key,command = value[0]).grid(row=0,column=column)
 			column += 1
 
-	def create_labels(self,frame,options):
+	def create_main_labels(self,frame,options):
 		row=3
 		for name,desc in options:
 			tk.Label(frame,text=name,width=15).grid(row=row,column=1)
 			tk.Label(frame,text=desc,width=65,wraplength=300).grid(row=row,column=2)
 			row+=1
+
+	def create_labels(self,frame,options):
+		current = 1
+		keys = options.keys()
+		width = 80 / len(keys)
+		while current != len(keys)+1:
+			row=2
+			tk.Label(frame,text=keys[current-1],bd=3,relief='raised',width=width,pady=3).grid(row=1,column=current)
+			for value in options[keys[current-1]]:
+				tk.Label(frame,text=value,bd=1,relief='raised',width=width,pady=3).grid(row=row,column=current)
+				row += 1
+			current += 1
 
 	def main_view(self):
 		self.master.title('DB Explorer')
@@ -80,37 +99,81 @@ class Gui(tk.Frame):
 
 		tk.Label(self.main_expl_display,text='Welcome to DB Explorer',width=20).grid(row=1,column=1,columnspan=2)
 		tk.Label(self.main_expl_display,text='').grid(row=2,column=1)
-		self.create_labels(self.main_expl_display,self.main_explanations)
+		self.create_main_labels(self.main_expl_display,self.main_explanations)
 
 	def db_view(self):
 		self.master.title('DB')
 		'''create inside frames and pack them'''
 		self.db_buttons_display = tk.Frame(self.db_display)
-		self.db_expl_display = tk.Frame(self.db_display)
+		self.inner_db_display = tk.Frame(self.db_display,bd=2,relief='raised')
 		self.db_display.pack()
 		self.db_buttons_display.pack(anchor='n')
-		self.db_expl_display.pack()
-
 
 		self.create_buttons(self.db_buttons_display,self.db_options)
+
+	def show_dbs(self):
+		self.clean_inner_frame(self.inner_db_display)
+		to_display = self.explorer.show_dbs()
+		self.create_labels(self.inner_db_display,to_display)
 
 	def table_view(self):
 		self.master.title('Table')
 		'''create inside frames and pack them'''
 		self.table_buttons_display = tk.Frame(self.table_display)
-		self.table_expl_display = tk.Frame(self.table_display)
+		self.inner_parent_display = tk.Frame(self.table_display)
+		self.inner_table_display = tk.Frame(self.inner_parent_display,bd=2,relief='raised')
+		self.inner_options_display = tk.Frame(self.inner_parent_display)
 		self.table_display.pack()
 		self.table_buttons_display.pack(anchor='n')
-		self.table_expl_display.pack()
-
-
+		self.inner_parent_display.pack()
 		self.create_buttons(self.table_buttons_display,self.table_options)
+
+	def show_tables(self):
+		self.clean_inner_parent()
+		self.inner_table_display.pack()
+		self.inner_table_display.config(bd=2)
+		to_display = self.explorer.show_tables()
+		self.create_labels(self.inner_table_display,to_display)
+
+	def view_table(self):
+		self.clean_inner_parent()
+		self.inner_options_display.pack()
+		tables = self.explorer.show_tables()['Name']
+		tk.OptionMenu(self.inner_options_display,self.table_var,*tables).grid(row=1,column=1)
+		tk.Button(self.inner_options_display,text='Search',command = self.search_table).grid(row=1,column=2)
+
+	def search_table(self):
+		self.clean_inner_frame(self.inner_table_display)
+		self.inner_table_display.config(bd=2)
+		results = self.explorer.show_table(self.table_var.get())
+		self.create_labels(self.inner_table_display,results)
+
+
+	def clean_inner_frame(self,frame):
+		for child in frame.winfo_children():
+			child.destroy()
+		frame.pack()
+
+	def clean_inner_parent(self):
+		for child in self.inner_parent_display.winfo_children():
+			if isinstance(child,tk.Frame):
+				child.configure(bd=0)
+				try:
+					child.pack_forget()
+				except:
+					child.grid_forget()
+				finally:
+					for ch in child.winfo_children():
+						ch.destroy()
+			else:
+				child.destroy()
 
 	def packer(self,to_unpack,to_pack):
 		for child in to_unpack.winfo_children():
 			child.destroy()
 		to_unpack.pack_forget()
 		to_pack()
+
 
 	def quit(self):
 		self.master.quit()
