82bcd39dff9fcc71be468c19cf2b56487affecfc
SuburbanFilth <suburb4nfilth@gmail.com>
2015-08-24 23:59:12 +0300

    Added some documentation
    Adding docstrings and comments to the code

--- README.md
+++ README.md
@@ -9,10 +9,3 @@ MySQL - http://dev.mysql.com/downloads/
 MySQLdb - https://pypi.python.org/pypi/MySQL-python or use pip
 
 Pexpect - https://pypi.python.org/pypi/pexpect/ or use pip :)
-
-TODO :
-
-1. Add more serious check on the default value, and add perhaps extra field
-2. Add ORDER BY, WHERE etc
-3. Add Foreign Keys
-5. add option to change the database from the gui and add the gui for the root password and username
--- explorer.py
+++ explorer.py
@@ -15,64 +15,92 @@ except ImportError:
 class Explorer(object):
 
 	def __init__(self,root_username,root_password):
-		#get the username for the root user, needed for the connection object
+		#username and password to start the mysql server
 		self.root_user = root_username
-		#get the password for the connection object and to start the server
 		self.root_pass = root_password
 		#start the mysql server
 		self.start_server()
 		#create the connection
+		#using a try inside a try
+		#because for some reason if i use time.sleep after the start server it fails
 		try:
-			#using a try except block because on the first try it always fails, i need to figure out why
+			#connecting the to the database
+			#since this is a db explorer and is not meant to manage dbs
+			#it uses the localhost and the existing test db
 			self.connection = MySQLdb.connect(host='',
 											user = self.root_user,
 											passwd = self.root_pass,
 											db='test')
 		except MySQLdb.Error:
-			#as it fails on the first try, we are trying again and this time wait a few seconds because otherwaise raises the error even when the DB has started
+			#if the connection fails the first time we try again
+			#we need to because it starts the server really slow (atleast on my mac( new computer calling))
 			try:
+				#using time.sleep afterall, here it doesn't fail though..
 				time.sleep(4)
 				self.connection = MySQLdb.connect(host='',
 												user = self.root_user,
 												passwd = self.root_pass,
 												db='test')
 			except MySQLdb.Error:
+				#if it raises another Error its probably 
+				#because of the credentials or because 
+				#the users computer is slower than mine (new computer calling v.2))
 				raise ex.NoMYSQL()
-
+		#CURSOR ASSEMBLE!
 		self.cursor = self.connection.cursor()	
 
 
 	def create_table(self,info_dict):
+		''' info_dict - dict containing the table name and the names of the columns
+
+		'''
+		#using try except to catch the mysql errors that might be raised
+		#currently all the raised error at this level are catched and TableExist error is raised
 		try:
+			#statement to get the number of tables currently in the db
 			desc_statement = 'SHOW TABLES'
 			self.cursor.execute(desc_statement)
 			table_num = [x[0] for x in self.cursor.fetchall()]
+			#check the number of tables in the database
+			#currently the  max number is 5 to avoid displaying issues
+			#Perhaps usa a var instead of hardcoding the number ?
 			if len(table_num) < 5:
-				sentinel = 2
+				#use a var to determain if its the last value and weather a comma should be added
+				comma_var = 2
+				#create the initial part of the statement using format because these arguments
+				#cannot be added with the standard argument passing to the execute function
+				#it's not safe but currently i dont know any other way
 				statement_start = 'CREATE TABLE {table_name} ('.format(table_name = info_dict['table_name'])
 				statement_end = ')'
 				statement_mid = ''
 				items = info_dict.items()
+				#iterate over the the columns in the info dict
 				for key,value in items:
+					#we dont need the table name when constructing the mid statement
 					if key != 'table_name':
+						#prepare the default value if one is selected
 						if value[0] == 'VARCHAR':
 							if value[3] != '':
 								value[3] = 'DEFAULT "{}"'.format(value[3])
 						else:
 							if value[3] != '':
 								value[3] = 'DEFAULT {}'.format(value[3])
-						if sentinel == len(info_dict):
+						#make sure to add a coma if its not the last col
+						if comma_var == len(info_dict):
 							statement_mid += '{col_name} {col_type}({col_size}) {null} {default} '.format(col_name = key ,col_type = value[0], col_size = value[1], null = value[2],default = value[3])
 						else:
 							statement_mid += '{col_name} {col_type}({col_size}) {null} {default} ,'.format(col_name = key  ,col_type = value[0], col_size = value[1], null=value[2],default=value[3])
-						sentinel += 1
+						comma_var += 1
+				#prepare the primary key constraint
+				#currently this is the only way to add a PK
+				#  - check the differences between this and the normal last value in the col info PK addidng
 				pk_list = [key for key,value in items if key != 'table_name' and value[4] == 'Yes']
-				if len(pk_list) > 1:
+				if len(pk_list) > 0:
 					statement_mid += ',CONSTRAINT pk_{} PRIMARY KEY ({})'.format(info_dict['table_name'],','.join(pk_list))
-				elif len(pk_list) == 1:
-					statement_mid += ',CONSTRAINT pk_{} PRIMARY KEY ({})'.format(info_dict['table_name'],pk_list[0])
+				#construct and execute the statement
 				final_statement = statement_start + statement_mid + statement_end
 				self.cursor.execute(final_statement)
+				#returning the statement for displaying
 				return final_statement
 			else:
 				raise ex.MaxTableNumberReached(5)
@@ -81,66 +109,98 @@ class Explorer(object):
 			raise ex.TableExists(info_dict['name'])
 
 	def view_table_info(self,table_name):
-		try:
-			statement = 'DESCRIBE %s' % table_name
-			self.cursor.execute(statement)
-			to_return = {'Field':[],'Type':[],'Null':[],'Key':[],'Default':[]}
-			records = self.cursor.fetchall()
-			for tupl in records:
-				to_return['Order'] = ('Field','Type','Null','Key','Default')
-				to_return['Field'].append(tupl[0])
-				to_return['Type'].append(tupl[1])
-				to_return['Null'].append(tupl[2])
-				to_return['Key'].append(tupl[3])
-				to_return['Default'].append(tupl[4])
-			to_return['query'] = statement
-			return to_return
-		except Exception:
-			raise ex.TableDoesntExist(table_name)
+		#query the db for the table info
+		statement = 'DESCRIBE %s' % table_name
+		self.cursor.execute(statement)
+		#the different fields that every columns contains, EXTRA is missing :(
+		options = ('Field','Type','Null','Key','Default')
+		to_return = {x:[] for x in options}
+		records = self.cursor.fetchall()
+		for tupl in records:
+			#preserve the order in which they should be displayed
+			#Ordered dict could be used aswell
+			to_return['Order'] = ('Field','Type','Null','Key','Default')
+			to_return['Field'].append(tupl[0])
+			to_return['Type'].append(tupl[1])
+			to_return['Null'].append(tupl[2])
+			to_return['Key'].append(tupl[3])
+			to_return['Default'].append(tupl[4])
+		to_return['query'] = statement
+		return to_return
 
 	def insert_into(self,table_name,values):
+		'''table_name - string 
+			values - list of values to be entered in the table
+		'''
+		#get all the rows in that table because we need their len, check todo.txt GLOBAL
 		all_values_statement = 'SELECT * FROM {}'.format(table_name)
 		self.cursor.execute(all_values_statement)
 		if len(self.cursor.fetchall()) < 10:
+			#using a try block to avoid checking the values
+			#delegating it to the database instead
 			try:
+				#querying the db for the column info
 				desc_statement = 'DESCRIBE {}'.format(table_name)
 				self.cursor.execute(desc_statement)
 				columns = self.cursor.fetchall()
+				#extracting the column names [0], the col types [1] and the null status [2]
 				column_names,types,nulls = [[x[index] for x in columns] for index in xrange(3)]
+				#preparing the statement
 				start_statement = 'INSERT INTO {} ('.format(table_name)
 				column_names = ','.join(column_names)
 				mid_statement = ') VALUES ('
+				#delegating the work to check and prepare the values to check_values
 				values = ','.join(self.check_values(values,types,nulls))
 				end_statement = ')'
-				final = start_statement+column_names+mid_statement+values+end_statement
-				self.cursor.execute(final)
-				return final
+				#prepare and execute the statement
+				final_statement = start_statement+column_names+mid_statement+values+end_statement
+				self.cursor.execute(final_statement)
+				#return the statement for displaying
+				return final_statement
 			except MySQLdb.Error as e:
+				#if a value is too big for the field catch the exception and raise more user friendly error
 				if e[0] == 1264:
 					raise ex.ValueTooBig()
+				#raise this if a user enters a chars in int field
 				elif e[0] == 1054:
 					raise ex.InvalidIntValue()
+				#raise this if a pk field uses already existing value
 				elif e[0] == 1062:
 					raise ex.PKDuplicate()
+				#for dev purposes, print any error meseges that i still havent seen
 				else:
 					print e
 		else:
 			raise ex.MaxRowReached()
 
-	def check_values(self,values,types,nulls):
-		for index in xrange(len(nulls)):
-			if nulls[index] == 'NO':
-				if values[index] == '':
-					raise ex.NullValue()
-			elif nulls[index] == 'YES':
-				if values[index] == '':
-					values[index] = 'NULL'
+	def check_values(self,values_list,types_list,nulls_list):
+		''' values_list = list of values to be prepared and checked
+			types_list = list of the the types of the said values
+			nulls_list = list that shows weather the values or NULL
 
-		how_many = [x for x in xrange(len(types)) if types[x].startswith('int') or values[x] == 'NULL']
-		values = ['"'+value+'"' if values.index(value) not in how_many else value for value in values]
-		return values
+		'''
+		#check the null values and insert them as NULL instead of an empty string 
+		#or raise a NullValue if there is no value in a primary key field
+		for index in xrange(len(nulls_list)):
+			if nulls_list[index] == 'NO':
+				if values_list[index] == '':
+					raise ex.NullValue()
+			elif nulls_list[index] == 'YES':
+				if values_list[index] == '':
+					values_list[index] = 'NULL'
+
+		#ccreating a list containing the indeces of the values that are int or are Null
+		how_many = [x for x in xrange(len(types_list)) if types_list[x].startswith('int') or values_list[x] == 'NULL']
+		#using the how_many list to determine if there should be quotes around the values or not
+		values_list = ['"'+value+'"' if values_list.index(value) not in how_many else value for value in values_list]
+		return values_list
 
 	def get_columns(self,table_name,option=None):
+		''' table_name - string
+			option - if its not none, it will return the types and the col names
+					else it will return only the names
+			CHECK todo.txt GLOBAL > 1.1
+		'''
 		statement = 'DESCRIBE {}'.format(table_name)
 		self.cursor.execute(statement)
 		fetched = self.cursor.fetchall()
@@ -151,25 +211,34 @@ class Explorer(object):
 		return columns
 
 	def show_tables(self):
+		''' check the todo.txt GLOBAL > 1.1 '''
 		statement = 'SHOW TABLES'
 		self.cursor.execute(statement)
 		to_return = {'Name': [x[0] for x in self.cursor.fetchall()]}
 		to_return['query'] = statement
+		#returns a dictionary containing the raw sql query and the table names
 		return to_return
 
 	def show_table(self,table_name):
+		''' table_name - string
+
+		'''
+		#get all values for the given table_name
 		statement = 'SELECT * FROM {}'.format(table_name)
 		to_return = {}
+		#execute describe to get the column names
 		column_statement = 'DESCRIBE {}'.format(table_name)
 		self.cursor.execute(column_statement)
-		checker = []
-		for tupl in self.cursor.fetchall():
-			to_return[tupl[0]] = []
-			checker.append(tupl[0])
+		column_info = self.cursor.fetchall()
 		self.cursor.execute(statement)
-		for tupl in self.cursor.fetchall():
-			for index in xrange(len(checker)):
-				to_return[checker[index]].append(tupl[index])
+		values = self.cursor.fetchall()
+		#iterate over the rows add them to the dictionary
+		for row in values:
+			for index in xrange(len(row)):
+				try:
+					to_return[column_info[index][0]].append(row[index])
+				except KeyError:
+					to_return[column_info[index][0]] = [row[index]]
 		to_return['query'] = statement
 		return to_return
 
